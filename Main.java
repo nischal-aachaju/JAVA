7d6069b253ef220cc8c227c494c7991e27876fda
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-20 18:31:22 +0545

    variables

--- Main.java
+++ Main.java
@@ -1,8 +1,29 @@
 public class Main {
     public static void main(String[]args)
     {
-        System.out.print("Hello world");
-        System.out.print("Nischal Shrsetha");
+        int value;
+        double pi=3.1415;
+        char a='y';
+        String he;
+        value=3;
+        he="hello";
+        boolean isStudent=true;
+
+        System.out.println("Hello world");
+        System.out.println("integer:"+value);
+        System.out.println("string:"+he);
+        System.out.println("double:"+pi);
+        System.out.println("char:"+a);
+        System.out.println("Boolean:"+isStudent);
+        if (isStudent){
+            
+            System.out.println("you are student");
+        
+        }
+        else{
+            System.out.println("you are not student");
+        }
+
     }
 
 }
