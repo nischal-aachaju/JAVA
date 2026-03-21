68cb583bd996dc9f186e6653238a015bb1515053
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-21 12:36:30 +0545

    arithmetic bitwise

---
+++ arithmetic.java
@@ -0,0 +1,8 @@
+public class arithmetic {
+    public static void main(String[] args){
+        int x=8; //1000
+        int y=2; //0010
+        int z =x^y; // bitwise //1010=10 
+        System.out.println(z); // output =10
+    }
+}
