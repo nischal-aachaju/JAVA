a7ecb6f169ab52124c6fc08dd28443fd86794ee2
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-24 18:04:41 +0545

    for loop and break,continue

--- ternary_operator.java
+++ ternary_operator.java
@@ -1,19 +1,26 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ternary_operator {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int age=14;
-        String canCastVote=(age>=18)?"You can vote":"You cannot vote";
-        System.out.println(canCastVote);
+        // Scanner scanner=new Scanner(System.in);
+        // int age=14;
+        // String canCastVote=(age>=18)?"You can vote":"You cannot vote";
+        // System.out.println(canCastVote);
         
-        String name= "";
-        while(name.isEmpty()){
-            System.out.print("Enter name: ");
-            name=scanner.nextLine();
+        // String name= "";
+        // while(name.isEmpty()){
+        //     System.out.print("Enter name: ");
+        //     name=scanner.nextLine();
+        // }
+        // System.out.print("Name:"+name);
+        
+        // scanner.close();
+
+        for (int i=0;i<5;i++){
+            if (i==3){
+                continue;
+            }
+            System.out.println("Hello"+i);
         }
-        System.out.print("Name:"+name);
-        
-        scanner.close();
     }
 }
